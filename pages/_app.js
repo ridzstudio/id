4f6a2732003fd7544b47da0bf4b9cf7ea56c394e
me-ridzstudio <me@ridzstudio.com>
2022-05-14 23:54:33 +0600

    _app.js beautified

--- pages/_app.js
+++ pages/_app.js
@@ -1,270 +1,30 @@
 import configureStore from './config/configureStore';
 import { Provider } from 'react-redux';
+import { Suspense, useState, useEffect } from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { ClimbingBoxLoader } from 'react-spinners';
+import { ThemeProvider } from '@material-ui/styles';
+import MuiTheme from './theme';
 import ScrollToTop from './utils/ScrollToTop';
 import './assets/base.scss';
 
+// Icons
+
 import { library } from '@fortawesome/fontawesome-svg-core';
-import {
-  fab,
-  faFacebook,
-  faTwitter,
-  faVuejs,
-  faReact,
-  faHtml5,
-  faGoogle,
-  faInstagram,
-  faPinterest,
-  faYoutube,
-  faDiscord,
-  faSlack,
-  faDribbble,
-  faGithub
-} from '@fortawesome/free-brands-svg-icons';
-import {
-  far,
-  faSquare,
-  faLifeRing,
-  faCheckCircle,
-  faTimesCircle,
-  faDotCircle,
-  faThumbsUp,
-  faComments,
-  faFolderOpen,
-  faTrashAlt,
-  faFileImage,
-  faFileArchive,
-  faCommentDots,
-  faFolder,
-  faKeyboard,
-  faCalendarAlt,
-  faEnvelope,
-  faAddressCard,
-  faMap,
-  faObjectGroup,
-  faImages,
-  faUser,
-  faLightbulb,
-  faGem,
-  faClock,
-  faUserCircle,
-  faQuestionCircle,
-  faBuilding,
-  faBell,
-  faFileExcel,
-  faFileAudio,
-  faFileVideo,
-  faFileWord,
-  faFilePdf,
-  faFileCode,
-  faFileAlt,
-  faEye,
-  faChartBar
-} from '@fortawesome/free-regular-svg-icons';
-import {
-  fas,
-  faExclamation,
-  faAngleDoubleRight,
-  faAngleDoubleLeft,
-  faCheck,
-  faSmile,
-  faHeart,
-  faBatteryEmpty,
-  faBatteryFull,
-  faChevronRight,
-  faSitemap,
-  faPrint,
-  faMapMarkedAlt,
-  faTachometerAlt,
-  faAlignCenter,
-  faExternalLinkAlt,
-  faShareSquare,
-  faInfoCircle,
-  faSync,
-  faQuoteRight,
-  faStarHalfAlt,
-  faShapes,
-  faCarBattery,
-  faTable,
-  faCubes,
-  faPager,
-  faCameraRetro,
-  faBomb,
-  faNetworkWired,
-  faBusAlt,
-  faBirthdayCake,
-  faEyeDropper,
-  faUnlockAlt,
-  faDownload,
-  faAward,
-  faPlayCircle,
-  faReply,
-  faUpload,
-  faBars,
-  faEllipsisV,
-  faSave,
-  faSlidersH,
-  faCaretRight,
-  faChevronUp,
-  faPlus,
-  faLemon,
-  faChevronLeft,
-  faTimes,
-  faChevronDown,
-  faFilm,
-  faSearch,
-  faEllipsisH,
-  faCog,
-  faArrowsAltH,
-  faPlusCircle,
-  faAngleRight,
-  faAngleUp,
-  faAngleLeft,
-  faAngleDown,
-  faArrowUp,
-  faArrowDown,
-  faArrowRight,
-  faArrowLeft,
-  faStar,
-  faSignOutAlt,
-  faLink
-} from '@fortawesome/free-solid-svg-icons';
-library.add(
-  far,
-  faSquare,
-  faLifeRing,
-  faCheckCircle,
-  faTimesCircle,
-  faDotCircle,
-  faThumbsUp,
-  faComments,
-  faFolderOpen,
-  faTrashAlt,
-  faFileImage,
-  faFileArchive,
-  faCommentDots,
-  faFolder,
-  faKeyboard,
-  faCalendarAlt,
-  faEnvelope,
-  faAddressCard,
-  faMap,
-  faObjectGroup,
-  faImages,
-  faUser,
-  faLightbulb,
-  faGem,
-  faClock,
-  faUserCircle,
-  faQuestionCircle,
-  faBuilding,
-  faBell,
-  faFileExcel,
-  faFileAudio,
-  faFileVideo,
-  faFileWord,
-  faFilePdf,
-  faFileCode,
-  faFileAlt,
-  faEye,
-  faChartBar
-);
-library.add(
-  fab,
-  faFacebook,
-  faTwitter,
-  faVuejs,
-  faReact,
-  faHtml5,
-  faGoogle,
-  faInstagram,
-  faPinterest,
-  faYoutube,
-  faDiscord,
-  faSlack,
-  faDribbble,
-  faGithub
-);
-library.add(
-  fas,
-  faExclamation,
-  faAngleDoubleRight,
-  faAngleDoubleLeft,
-  faCheck,
-  faSmile,
-  faHeart,
-  faBatteryEmpty,
-  faBatteryFull,
-  faChevronRight,
-  faSitemap,
-  faPrint,
-  faMapMarkedAlt,
-  faTachometerAlt,
-  faAlignCenter,
-  faExternalLinkAlt,
-  faShareSquare,
-  faInfoCircle,
-  faSync,
-  faQuoteRight,
-  faStarHalfAlt,
-  faShapes,
-  faCarBattery,
-  faTable,
-  faCubes,
-  faPager,
-  faCameraRetro,
-  faBomb,
-  faNetworkWired,
-  faBusAlt,
-  faBirthdayCake,
-  faEyeDropper,
-  faUnlockAlt,
-  faDownload,
-  faAward,
-  faPlayCircle,
-  faReply,
-  faUpload,
-  faBars,
-  faEllipsisV,
-  faSave,
-  faSlidersH,
-  faCaretRight,
-  faChevronUp,
-  faPlus,
-  faLemon,
-  faChevronLeft,
-  faTimes,
-  faChevronDown,
-  faFilm,
-  faSearch,
-  faEllipsisH,
-  faCog,
-  faArrowsAltH,
-  faPlusCircle,
-  faAngleRight,
-  faAngleUp,
-  faAngleLeft,
-  faAngleDown,
-  faArrowUp,
-  faArrowDown,
-  faArrowRight,
-  faArrowLeft,
-  faStar,
-  faSignOutAlt,
-  faLink
-);
-const store = configureStore();
-
-import React, { Suspense, useState, useEffect } from 'react';
-import { AnimatePresence, motion } from 'framer-motion';
-import { ClimbingBoxLoader } from 'react-spinners';
-
-import { ThemeProvider } from '@material-ui/styles';
-
-import MuiTheme from './theme';
+import { fab, faFacebook, faTwitter, faVuejs, faReact, faHtml5, faGoogle, faInstagram, faPinterest, faYoutube, faDiscord, faSlack, faDribbble, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { far, faSquare, faLifeRing, faCheckCircle, faTimesCircle, faDotCircle, faThumbsUp, faComments, faFolderOpen, faTrashAlt, faFileImage, faFileArchive, faCommentDots, faFolder, faKeyboard, faCalendarAlt, faEnvelope, faAddressCard, faMap, faObjectGroup, faImages, faUser, faLightbulb, faGem, faClock, faUserCircle, faQuestionCircle, faBuilding, faBell, faFileExcel, faFileAudio, faFileVideo, faFileWord, faFilePdf, faFileCode, faFileAlt, faEye, faChartBar } from '@fortawesome/free-regular-svg-icons';
+import { fas, faExclamation, faAngleDoubleRight, faAngleDoubleLeft, faCheck, faSmile, faHeart, faBatteryEmpty, faBatteryFull, faChevronRight, faSitemap, faPrint, faMapMarkedAlt, faTachometerAlt, faAlignCenter, faExternalLinkAlt, faShareSquare, faInfoCircle, faSync, faQuoteRight, faStarHalfAlt, faShapes, faCarBattery, faTable, faCubes, faPager, faCameraRetro, faBomb, faNetworkWired, faBusAlt, faBirthdayCake, faEyeDropper, faUnlockAlt, faDownload, faAward, faPlayCircle, faReply, faUpload, faBars, faEllipsisV, faSave, faSlidersH, faCaretRight, faChevronUp, faPlus, faLemon, faChevronLeft, faTimes, faChevronDown, faFilm, faSearch, faEllipsisH, faCog, faArrowsAltH, faPlusCircle, faAngleRight, faAngleUp, faAngleLeft, faAngleDown, faArrowUp, faArrowDown, faArrowRight, faArrowLeft, faStar, faSignOutAlt, faLink } from '@fortawesome/free-solid-svg-icons';
+library.add(far, faSquare, faLifeRing, faCheckCircle, faTimesCircle, faDotCircle, faThumbsUp, faComments, faFolderOpen, faTrashAlt, faFileImage, faFileArchive, faCommentDots, faFolder, faKeyboard, faCalendarAlt, faEnvelope, faAddressCard, faMap, faObjectGroup, faImages, faUser, faLightbulb, faGem, faClock, faUserCircle, faQuestionCircle, faBuilding, faBell, faFileExcel, faFileAudio, faFileVideo, faFileWord, faFilePdf, faFileCode, faFileAlt, faEye, faChartBar);
+library.add(fab, faFacebook, faTwitter, faVuejs, faReact, faHtml5, faGoogle, faInstagram, faPinterest, faYoutube, faDiscord, faSlack, faDribbble, faGithub);
+library.add(fas, faExclamation, faAngleDoubleRight, faAngleDoubleLeft, faCheck, faSmile, faHeart, faBatteryEmpty, faBatteryFull, faChevronRight, faSitemap, faPrint, faMapMarkedAlt, faTachometerAlt, faAlignCenter, faExternalLinkAlt, faShareSquare, faInfoCircle, faSync, faQuoteRight, faStarHalfAlt, faShapes, faCarBattery, faTable, faCubes, faPager, faCameraRetro, faBomb, faNetworkWired, faBusAlt, faBirthdayCake, faEyeDropper, faUnlockAlt, faDownload, faAward, faPlayCircle, faReply, faUpload, faBars, faEllipsisV, faSave, faSlidersH, faCaretRight, faChevronUp, faPlus, faLemon, faChevronLeft, faTimes, faChevronDown, faFilm, faSearch, faEllipsisH, faCog, faArrowsAltH, faPlusCircle, faAngleRight, faAngleUp, faAngleLeft, faAngleDown, faArrowUp, faArrowDown, faArrowRight, faArrowLeft, faStar, faSignOutAlt, faLink);
 
 // Layout Blueprints
 
 import { MinimalLayout } from './layout-blueprints';
+
+// Constants
+
+const store = configureStore();
 
 function App({ Component, pageProps }) {
   const pageVariants = {

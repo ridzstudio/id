855436a5c27736077641e1bb16bb4423c263b951
me-ridzstudio <me@ridzstudio.com>
2022-05-15 05:49:27 +0600

    Ridz Studio branding added on sign up page

--- pages/sign-up.js
+++ pages/sign-up.js
@@ -1,4 +1,6 @@
 import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
 
 import { Grid, Button, TextField } from '@material-ui/core';
 
@@ -9,92 +11,101 @@ export default function SignUp() {
         <title>Sign up page template</title>
       </Head>
       <main>
-      <div className="app-wrapper bg-white min-vh-100">
-        <div className="app-main min-vh-100">
-          <div className="app-content p-0">
-            <div className="app-content--inner d-flex align-items-center">
-              <div className="flex-grow-1 w-100 d-flex align-items-center">
-                <div className="bg-composed-wrapper--content py-5">
-                  <Grid item md={10} lg={8} xl={4} className="mx-auto">
-                    <div className="text-center mb-4">
-                      <h1 className="display-4 mb-1 font-weight-bold">
-                        Create your account
-                      </h1>
-                      <p className="font-size-lg mb-0 text-black-50">
-                        Start benefiting from our tools right away
-                      </p>
-                    </div>
-                    <div className="mb-3">
-                      <label className="font-weight-bold mb-2">
-                        Email address
-                      </label>
-                      <TextField
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        placeholder="Enter your email address"
-                        type="email"
-                      />
-                    </div>
-                    <div className="mb-3">
-                      <div className="d-flex justify-content-between">
-                        <label className="font-weight-bold mb-2">
-                          Password
-                        </label>
+        <div className="app-wrapper bg-white min-vh-100">
+          <div className="app-main min-vh-100">
+            <div className="app-content p-0">
+              <div className="app-content--inner d-flex align-items-center">
+                <div className="flex-grow-1 w-100 d-flex align-items-center">
+                  <div className="bg-composed-wrapper--content py-5">
+                    <Grid item md={10} lg={8} xl={4} className="mx-auto">
+                      <div className="text-center mb-4">
+                        <div className='mb-4'>
+                          <Image
+                            src='/logo.svg'
+                            alt='Ridz Studio'
+                            width={120}
+                            height={70}
+                          />
+                        </div>
+                        <h1 className="display-4 mb-1 font-weight-bold">
+                          Create your Ridz Studio ID
+                        </h1>
+                        <p className="font-size-lg mb-0 text-black-50">
+                          One Ridz Studio ID is all you need to access all Ridz Studio services. <br/>
+                          Already have an Ridz Studio ID? <Link href="/sign-in"><a>Find it here</a></Link>
+                        </p>
                       </div>
-                      <TextField
-                        variant="outlined"
-                        size="small"
-                        fullWidth
-                        placeholder="Enter your password"
-                        type="password"
-                      />
-                    </div>
-                    <Grid container spacing={6}>
-                      <Grid item md={6}>
-                        <div>
+                      <div className="mb-3">
+                        <label className="font-weight-bold mb-2">
+                          Email address
+                        </label>
+                        <TextField
+                          variant="outlined"
+                          size="small"
+                          fullWidth
+                          placeholder="Enter your email address"
+                          type="email"
+                        />
+                      </div>
+                      <div className="mb-3">
+                        <div className="d-flex justify-content-between">
                           <label className="font-weight-bold mb-2">
-                            First name
+                            Password
                           </label>
-                          <TextField
-                            variant="outlined"
-                            size="small"
-                            fullWidth
-                            placeholder="Enter your first name"
-                          />
                         </div>
+                        <TextField
+                          variant="outlined"
+                          size="small"
+                          fullWidth
+                          placeholder="Enter your password"
+                          type="password"
+                        />
+                      </div>
+                      <Grid container spacing={6}>
+                        <Grid item md={6}>
+                          <div>
+                            <label className="font-weight-bold mb-2">
+                              First name
+                            </label>
+                            <TextField
+                              variant="outlined"
+                              size="small"
+                              fullWidth
+                              placeholder="Enter your first name"
+                            />
+                          </div>
+                        </Grid>
+                        <Grid item md={6}>
+                          <div>
+                            <label className="font-weight-bold mb-2">
+                              Last name
+                            </label>
+                            <TextField
+                              variant="outlined"
+                              size="small"
+                              fullWidth
+                              placeholder="Enter your last name"
+                            />
+                          </div>
+                        </Grid>
                       </Grid>
-                      <Grid item md={6}>
-                        <div>
-                          <label className="font-weight-bold mb-2">
-                            Last name
-                          </label>
-                          <TextField
-                            variant="outlined"
-                            size="small"
-                            fullWidth
-                            placeholder="Enter your last name"
-                          />
-                        </div>
-                      </Grid>
+                      <div className="my-4">
+                        By clicking the <strong>Create account</strong> button
+                        below you agree to our terms of service and privacy
+                        statement.
+                      </div>
+                      <div className="text-center mb-4">
+                        <Button className="btn-primary text-uppercase font-weight-bold font-size-sm my-3">
+                          Create account
+                        </Button>
+                      </div>
                     </Grid>
-                    <div className="my-4">
-                      By clicking the <strong>Create account</strong> button
-                      below you agree to our terms of service and privacy
-                      statement.
-                    </div>
-                    <div className="text-center mb-4">
-                      <Button className="btn-primary text-uppercase font-weight-bold font-size-sm my-3">
-                        Create account
-                      </Button>
-                    </div>
-                  </Grid>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
       </main>
     </>
   )
